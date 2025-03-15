4609608123b5f10db11199eb4dcd51b279f0f451
Олег <bonbarcovoleg55@gmail.com>
2025-03-15 17:30:29 +0300

    +

---
+++ Assets/Practice/peremen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class peremen : MonoBehaviour
+{
+    /// переменные
+    /// 
+    int score = 10;
+    float speed = 5.5f;
+    string playerName = "Oleg";
+    bool isAlive = true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
